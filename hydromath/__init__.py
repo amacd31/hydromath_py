8d4f9bfd71b159efe6781b9faa7db67b90012491
Andrew MacDonald <andrew@maccas.net>
2016-03-08 18:58:30 +1100

    Expose RMSE and NSE functions in addition to MSE

--- hydromath/__init__.py
+++ hydromath/__init__.py
@@ -15,11 +15,29 @@ __lib = __ffi.dlopen(os.path.join(PKG_ROOT, 'hydromath.so'))
 
 mse_c = __lib.mse_c
 
-def mse(obs, sim):
-    assert len(obs) == len(sim)
+def __map_func(c_func_name, obs, sim):
+    """
+        Map to a Rust function via the C interface.
 
-    return mse_c(
+        Since all the skill score functions take two arrays (and a length) we
+        can avoid repeating code by following the required pattern in one
+        place. This function looks up the c/rust function name based on the
+        passed argument then passes the converted data into the function. Later
+        versions can also handle ensuring the passed arrays are numpy arrays of
+        the correct data type.
+    """
+    assert len(obs) == len(sim)
+    return __lib.__getattr__(c_func_name)(
         __ffi.cast('double *', obs.ctypes.data),
         __ffi.cast('double *', sim.ctypes.data),
         len(obs)
     )
+
+def mse(obs, sim):
+    return __map_func('mse_c', obs, sim)
+
+def rmse(obs, sim):
+    return __map_func('rmse_c', obs, sim)
+
+def nse(obs, sim):
+    return __map_func('nse_c', obs, sim)

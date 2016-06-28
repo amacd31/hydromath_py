8e6f80631fc6cd3f240199e98afc802f13225f82
Andrew MacDonald <andrew@maccas.net>
2016-06-28 23:17:26 +1000

    Add unit tests

---
+++ tests/test_mse.py
@@ -0,0 +1,38 @@
+import unittest
+
+import numpy as np
+
+from hydromath import mse
+
+class MSETestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.test_obs = np.array([13.,17.,18.,20.,24.])
+        self.test_sim = np.array([12.,15.,20.,22.,24.])
+
+    def tearDown(self):
+        pass
+
+    def test_mse(self):
+        result = mse(self.test_obs, self.test_sim)
+
+        self.assertEqual(result, 2.6)
+
+    def test_mse_perfect(self):
+        result = mse(np.array([1.,2.,3.,4.,5.]),
+            np.array([1.,2.,3.,4.,5.]))
+
+        self.assertEqual(result, 0)
+
+    def test_mse_bad(self):
+        m = np.mean([1.,2.,3.,4.,5.])
+        sim = np.array([m, m, m, m, m])
+        result = mse(np.array([1.,2.,3.,4.,5.]),
+                    sim
+                )
+
+        self.assertEqual(result, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_nse.py
@@ -0,0 +1,39 @@
+import unittest
+
+import numpy as np
+
+from hydromath import nse
+
+class NSETestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.test_obs = np.array([5,4,6,1,3,6,8,1,7,3,4,0.5])
+        self.test_sim = np.array([3,4.5,4,2,4,5,9,2,8,3,4,0.8])
+
+    def tearDown(self):
+        pass
+
+    def test_nse(self):
+        result = nse(self.test_obs, self.test_sim)
+
+        self.assertEqual(result, 0.783479081472161)
+
+    def test_nse_perfect(self):
+        result = nse(np.array([1.,2.,3.,4.,5.]),
+                np.array([1.,2.,3.,4.,5.]))
+
+        self.assertEqual(result, 1)
+
+    def test_nse_bad(self):
+        m = np.mean([1.,2.,3.,4.,5.])
+        sim = np.array([m, m, m, m, m])
+        result = nse(np.array([1.,2.,3.,4.,5.]),
+                    sim
+                )
+
+        # Score of zero when no better than the mean.
+        self.assertEqual(result, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()

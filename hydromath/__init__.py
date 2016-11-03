6be42ed2f83585934c5411c6546d36863f545c83
Andrew MacDonald <andrew@maccas.net>
2016-11-03 22:59:26 +1100

    Use versioneer

--- hydromath/__init__.py
+++ hydromath/__init__.py
@@ -1,1 +1,5 @@
 from hydromath._math import mse, rmse, nse, kge, heaviside
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
+import versioneer
 
 setup(name='hydromath',
-    version='0.0.1',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     packages=['hydromath'],
     install_requires=['cffi>=1.0.0'],
     cffi_modules=["hydromath/_build.py:ffi"],
